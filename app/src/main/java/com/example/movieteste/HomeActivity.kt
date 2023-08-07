e6612b216b6edddee68d234066870fdc6b3bd95d
leonardomarques <leonardomarques84@hotmail.com>
2023-08-07 14:33:02 -0300

    created list top reated

--- app/src/main/java/com/example/movieteste/HomeActivity.kt
+++ app/src/main/java/com/example/movieteste/HomeActivity.kt
@@ -6,8 +6,9 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.movieteste.adapters.MovieAdapter
 import com.example.movieteste.model.Movie
 import com.example.movieteste.model.MovieResponse
-import com.example.movieteste.service.MovieApiInterface
+import com.example.movieteste.service.MoviePopularApiInterface
 import com.example.movieteste.service.MovieApiService
+import com.example.movieteste.service.MovieTopreatedApiInterface
 import kotlinx.android.synthetic.main.activity_home.*
 import retrofit2.Call
 import retrofit2.Callback
@@ -19,15 +20,22 @@ class HomeActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
 
-        rv_movies_list.layoutManager = LinearLayoutManager(this)
-        rv_movies_list.setHasFixedSize(true)
+        rv_popular_list.layoutManager = LinearLayoutManager(this)
+        rv_popular_list.setHasFixedSize(true)
         getMovieData { movies : List<Movie> ->
-            rv_movies_list.adapter = MovieAdapter(movies)
+            rv_popular_list.adapter = MovieAdapter(movies)
         }
+
+        rv_top_reated_list.layoutManager = LinearLayoutManager(this)
+        rv_top_reated_list.setHasFixedSize(true)
+        getMovieTopData { movies : List<Movie> ->
+            rv_top_reated_list.adapter = MovieAdapter(movies)
+        }
+
     }
 
     private fun getMovieData(callback: (List<Movie>) -> Unit){
-        val apiService = MovieApiService.getInstance().create(MovieApiInterface::class.java)
+        val apiService = MovieApiService.getInstance().create(MoviePopularApiInterface::class.java)
         apiService.getMovieList().enqueue(object : Callback<MovieResponse> {
             override fun onFailure(call: Call<MovieResponse>, t: Throwable) {
 
@@ -39,4 +47,19 @@ class HomeActivity : AppCompatActivity() {
 
         })
     }
+
+    private fun getMovieTopData(callback: (List<Movie>) -> Unit){
+        val apiService = MovieApiService.getInstance().create(MovieTopreatedApiInterface::class.java)
+        apiService.getMovieList().enqueue(object : Callback<MovieResponse> {
+            override fun onFailure(call: Call<MovieResponse>, t: Throwable) {
+
+            }
+
+            override fun onResponse(call: Call<MovieResponse>, response: Response<MovieResponse>) {
+                return callback(response.body()!!.movies)
+            }
+
+        })
+    }
+
 }
--- app/src/main/java/com/example/movieteste/service/MoviePopularApiInterface.kt
+++ app/src/main/java/com/example/movieteste/service/MoviePopularApiInterface.kt
@@ -4,7 +4,7 @@ import com.example.movieteste.model.MovieResponse
 import retrofit2.Call
 import retrofit2.http.GET
 
-interface MovieApiInterface {
+interface MoviePopularApiInterface {
 
     @GET("/3/movie/popular?api_key=ed14716f0a92f2daa0ea788dd2b7dd81")
     fun getMovieList(): Call<MovieResponse>
@@ -12,3 +12,9 @@ interface MovieApiInterface {
 
 }
 
+interface MovieTopreatedApiInterface {
+
+    @GET("/3/movie/top_rated?api_key=ed14716f0a92f2daa0ea788dd2b7dd81")
+    fun getMovieList(): Call<MovieResponse>
+
+}
